2ca3695d81d2d7c6fc0731d446487f28e88febe2
Benjamin <benjamin.barnel@qapa.com>
2022-11-24 16:05:18 +0100

    First commit

---
+++ src/main/java/com/qapa/sandpit/SandpitApplication.java
@@ -0,0 +1,13 @@
+package com.qapa.sandpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandpitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandpitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qapa/sandpit/SandpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qapa.sandpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandpitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
